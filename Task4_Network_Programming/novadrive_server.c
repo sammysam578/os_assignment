0d54e0c52f8a3531ef87d5b25916ed86623d4625
sammysam578 <gysammy02@gmail.com>
2026-07-15 19:11:22 +0545

    Create NovaDrive TCP server

---
+++ Task4_Network_Programming/novadrive_server.c
@@ -0,0 +1,65 @@
+/*
+    NovaDrive Network Programming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    // Server socket descriptor
+    int serverSocket;
+
+    // Server and client address structures
+    struct sockaddr_in serverAddress;
+
+    // Create TCP socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(serverSocket < 0)
+    {
+        printf("Socket Creation Failed.\n");
+        return 1;
+    }
+
+    printf("Server Socket Created Successfully.\n");
+
+    // Configure server address
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    // Bind socket to port
+    if(bind(serverSocket,
+            (struct sockaddr *)&serverAddress,
+            sizeof(serverAddress)) < 0)
+    {
+        printf("Bind Failed.\n");
+        close(serverSocket);
+        return 1;
+    }
+
+    printf("Server Bound To Port %d\n", PORT);
+
+    // Listen for incoming connections
+    if(listen(serverSocket, 5) < 0)
+    {
+        printf("Listen Failed.\n");
+        close(serverSocket);
+        return 1;
+    }
+
+    printf("NovaDrive Server Waiting For Client...\n");
+
+    // Close socket
+    close(serverSocket);
+
+    return 0;
+}
